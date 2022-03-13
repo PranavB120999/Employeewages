b95082aea3250bab12229152cdfbae1549519286
Pranav <pranavbhople99@gmail.com>
2022-03-13 19:41:25 +0530

    file is added present and absent

---
+++ Employewage/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employewage
+{
+    internal class Class1
+    {
+        public static void UC1()
+        {
+            Console.WriteLine("Wlcome to UC_1");
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
---
+++ Employewage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Employeewages problem ");
+            Class1.UC1();
+        }
+    }
+}
